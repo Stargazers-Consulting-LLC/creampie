f7b5e8c22796b559ccc6204e62640467872f0521
Robert Ferguson <rmferguson@protonmail.com>
2025-06-15 11:49:52 -0700

    Swap to postgres

--- cream_api/db.py
+++ cream_api/db.py
@@ -1,12 +1,19 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+if __debug__:
+    SQLALCHEMY_DATABASE_URL = "postgresql://creamapp:creamapp@localhost/cream"
+else:
+    SQLALCHEMY_DATABASE_URL = os.getenv("POSTGRESQL_URL", "")
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+if not SQLALCHEMY_DATABASE_URL:
+    raise RuntimeError("Missing connection arguments: SQLALCHEMY_DATABASE_URL")
+
+assert isinstance(SQLALCHEMY_DATABASE_URL, str)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 ModelBase = declarative_base()
